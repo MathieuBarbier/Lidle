936cb2c13867d522b811baefe46f99ecf6106f3d
Antoni Pacciani <a.pacciani@vertic.al>
2015-07-03 21:45:34 +0200

    scene game + girls UI

---
+++ Assets/Scripts/GirlPanel.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GirlPanel : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void goLeft() {
+		animator.SetTrigger("goLeft");
+	}
+
+	public void goRight() {
+		animator.SetTrigger("goRight");
+	}
+
+	public void setRight() {
+		animator.SetTrigger("setRight");
+	}
+
+	public void comeFromRight() {
+		animator.SetTrigger("comeFromRight");
+	}
+
+	public void comeFromLeft() {
+		animator.SetTrigger("comeFromLeft");
+	}
+}
